5d10dc0d5f2dffb15e0887df554ee45802ffa788
Schraere <156011289+Schraere@users.noreply.github.com>
2024-04-11 15:42:27 -0500

    Update AltTextApp.py

--- AltTextApp.py
+++ AltTextApp.py
@@ -25,9 +25,9 @@ with result:
         for candidate in response.candidates:
             st.write(part.text for part in candidate.content.parts)
 user_text = st.text_input("Provide additional context")
-if user_text is not None:
-    model = genai.GenerativeModel('gemini-pro-vision')
-    response = model.generate_content(["Create alt-text for each image that meets WCAG 2.2 specifications. Suggest a short description for a caption.", image, user_text], stream=True)
-    response.resolve()
-    for candidate in response.candidates:
-        st.write(part.text for part in candidate.content.parts)
+    if user_text is not None:
+        model = genai.GenerativeModel('gemini-pro-vision')
+        response = model.generate_content(["Create alt-text for each image that meets WCAG 2.2 specifications. Suggest a short description for a caption.", image, user_text], stream=True)
+        response.resolve()
+        for candidate in response.candidates:
+            st.write(part.text for part in candidate.content.parts)
